562aa52304e9fca66c5c661e0e86ff48e791df30
GevorgyanSam <mylaptopacer.01182005@gmail.com>
2022-10-27 22:08:59 +0300

    Added The Search Bar For Finding Users BY 'Name, Last Name or Login'

--- sql/users.sql
+++ sql/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 26 2022 г., 13:10
+-- Время создания: Окт 27 2022 г., 21:07
 -- Версия сервера: 10.4.25-MariaDB
 -- Версия PHP: 8.1.10
 
@@ -15,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `test`
@@ -29,17 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `last_name` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `login` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `image` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `gen` varchar(7) CHARACTER SET utf8 NOT NULL,
-  `month` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `gen` varchar(7) NOT NULL,
+  `month` varchar(255) NOT NULL,
   `day` int(3) NOT NULL,
   `year` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `last_name`, `email`, `login`, `password`, `image`, `gen`, `month`, `day`, `year`) VALUES
+(1, 'Samvel', 'Gevorgyan', 'Gevorgyan@mail.ru', 'GSam', 'd91ac493291efec9ef943999ff4297a6', 'Sam.jpg', 'Male', 'January', 18, 2005),
+(2, 'Samvel', 'Hayrapetyan', 'Hayrapetyan@mail.ru', 'Sami', '83374adc2060a1d85245bcc293f28e3d', 'Sam.jpg', 'Male', 'April', 10, 2018),
+(3, 'Stepan', 'Hovsepyan', 'Hovsepyan@gmail.com', 'Step', '4060ebec0f8c118b9c92d91966b901d8', 'Sam.jpg', 'Male', 'July', 5, 2013),
+(4, 'Harut', 'Farfaryan', 'Farfaryan@mail.ru', 'Har', 'e02dde4b4d9a1bab0ecfc27cce3b8fb0', 'Sam.jpg', 'Male', 'February', 2, 2020);
 
 --
 -- Индексы сохранённых таблиц
@@ -59,7 +69,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- welcome.php
+++ welcome.php
@@ -20,6 +20,39 @@
     $u_day = $record["day"];
 ?>
 
+<?php
+
+    $search = $search_err = "";
+
+    if(isset($_POST["submit"])) {
+
+        if(empty($_POST["search"])) {
+            $search_err = "Enter The User Name or Login";
+        } else {
+            $search = $_POST["search"];
+            $search = htmlspecialchars($search);
+            $search = trim($search);
+            $search = stripslashes($search);
+        }
+
+        if(!empty($search)) {
+            $result = mysqli_query($conn, "SELECT * FROM `users` WHERE `users` . `name` LIKE '%$search%' OR `users` . `login` = '$search' OR `users` . `last_name` LIKE '%$search%' ");
+
+            if(mysqli_num_rows($result) > 0) {
+                $arr = [];
+                while($row = mysqli_fetch_assoc($result)) {
+                    $search_user_id = $row["id"];
+                    array_push($arr, $search_user_id);
+                }
+            } else {
+                $search_err = "The User Not Found";
+            }
+        }
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,7 +61,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo("$u_name $u_last_name") ?></title>
     <style>
-        body {
+        .parent {
             display: flex;
         }
 
@@ -71,54 +104,109 @@
         a {
             font-size: 25px;
         }
+
+        .searchParent {
+            margin-left: 40px;
+        }
+
+        .search_err {
+            margin-top: 20px;
+            color: red;
+        }
     </style>
 </head>
 <body>
-    <table border="1">
-        <thead>
-            <tr>
-                <th colspan="2">User Data</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Name: </td>
-                <td><?php echo($u_name); ?></td>
-            </tr>
-            <tr>
-                <td>Last Name: </td>
-                <td><?php echo($u_last_name); ?></td>
-            </tr>
-            <tr>
-                <td>Email: </td>
-                <td><?php echo($u_email); ?></td>
-            </tr>
-            <tr>
-                <td>Login: </td>
-                <td><?php echo($u_login); ?></td>
-            </tr>
-            <tr>
-                <td>Password: </td>
-                <td></td>
-            </tr>
-            <tr>
-                <td>Image: </td>
-                <td>
-                    <img src="img/<?=$u_image?>">
-                </td>
-            </tr>
-            <tr>
-                <td>Gen: </td>
-                <td><?php echo($u_gen); ?></td>
-            </tr>
-            <tr>
-                <td>Date: </td>
-                <td><?php echo($u_month . " " . $u_day . " " . $u_year); ?></td>
-            </tr>
-        </tbody>
-    </table>
-    <div class="logoutParent">
-        <a href="logout.php">Log Out</a>
+    <div class="parent">
+        <table border="1">
+            <thead>
+                <tr>
+                    <th colspan="2">User Data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name: </td>
+                    <td><?php echo($u_name); ?></td>
+                </tr>
+                <tr>
+                    <td>Last Name: </td>
+                    <td><?php echo($u_last_name); ?></td>
+                </tr>
+                <tr>
+                    <td>Email: </td>
+                    <td><?php echo($u_email); ?></td>
+                </tr>
+                <tr>
+                    <td>Login: </td>
+                    <td><?php echo($u_login); ?></td>
+                </tr>
+                <tr>
+                    <td>Password: </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Image: </td>
+                    <td>
+                        <img src="img/<?=$u_image?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Gen: </td>
+                    <td><?php echo($u_gen); ?></td>
+                </tr>
+                <tr>
+                    <td>Date: </td>
+                    <td><?php echo($u_month . " " . $u_day . " " . $u_year); ?></td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="logoutParent">
+            <a href="logout.php">Log Out</a>
+        </div>
+        <div class="searchParent">
+            <form method="POST" autocomplete="off">
+                <input type="text" placeholder="Name, Last Name or Login" name="search" value="<?=$search?>">
+                <button type="submit" name="submit">Search</button>
+                <div class="search_err">
+                    <?=$search_err?>
+                </div>
+                <?php if(isset($arr)): ?>
+                    <div class="users">
+                    <table border="1" style="width: 800px;">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Last Name</th>
+                                <th>Email</th>
+                                <th>Login</th>
+                                <th>Image</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php for($i = 0; $i <= count($arr) - 1; $i++) { ?>
+                                <?php
+                                    $find = $arr[$i];
+                                    $result = mysqli_query($conn, "SELECT * FROM `users` WHERE `users` . `id` = '$find'");
+                                    $row = mysqli_fetch_assoc($result);
+                                ?>
+                                <tr>
+                                    <td><?=$row["name"]?></td>
+                                    <td><?=$row["last_name"]?></td>
+                                    <td><?=$row["email"]?></td>
+                                    <td><?=$row["login"]?></td>
+                                    <td>
+                                        <img src="img/<?=$row["image"]?>">
+                                    </td>
+                                    <td><?=$row["month"] . " " . $row["day"] . " " . $row["year"]?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                    </div>
+                <?php endif ?>
+            </form>
+        </div>
     </div>
 </body>
 </html>
